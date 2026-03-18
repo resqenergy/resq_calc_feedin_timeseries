922f68fda684a39720e0854016102785ce098210
srhbrnds <sarah.berendes@rl-institut.de>
2026-03-18 15:00:26 +0100

    initial commit

---
+++ scripts/resq_preprocess_pv_timeseries.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from pathlib import Path
+
+DATA_DIR = Path(__file__).parent.parent / "data"
+RESULTS_DIR = Path(__file__).parent.parent / "results"
+PV_CONFIG_FILE=DATA_DIR/ "pv_config.csv"
+PV_TIMESERIES_FILE=DATA_DIR / "pv_feedin_ts.csv"
+
+pv_config=pd.read_csv(PV_CONFIG_FILE)
+pv_timeseries=pd.read_csv(PV_TIMESERIES_FILE,
+                          header=[0,1],
+                          parse_dates=True,
+                          index_col=0)
+
+pv_timeseries.columns = pd.MultiIndex.from_tuples(
+    [(int(t), int(a)) for t, a in pv_timeseries.columns],
+    names=["tilt", "azimuth"]
+)
+
+result = pd.DataFrame()
+
+for technology, group in pv_config.groupby("technology"):
+    ts = sum(
+        pv_timeseries[(row.tilt,row.azimuth)] * row.weight
+        for _, row in group.iterrows()
+    )
+    result[technology] = ts
+
+result.to_csv(RESULTS_DIR / "pv_feedin_try_rcp85_p3.csv")
+
